7fae3debdafe3b9679d092031c52ac1773a8510e
eomttt <hyunt0413@gmail.com>
2022-04-26 16:55:46 +0900

    :lipstick: Add tailwind CSS

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ yarn dev
 # Technical stack
 
 - Next JS
+- [Tailwind CSS](https://tailwindcss.com/docs/guides/nextjs)
 - Firebase
 - Deploy on [Vercel](https://vercel.com/)
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => (
-  <div>
-    <p>Hello world!</p>
-  </div>
-);
+const Home: NextPage = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 
 export default Home;
