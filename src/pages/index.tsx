e71db0274035276f1151d3c157cc6f763673a549
jsbugs <jsbugs.wrk@gmail.com>
2022-04-30 01:22:10 +0900

    index, subscribe pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,25 @@ import Link from 'next/link';
 
 const Home: NextPage = () => (
   <Layout>
-    <h1 className="w-full text-center text-6xl font-bold text-black-500 pt-10">NFT NOTICE</h1>
-    <h2 className="w-full text-center text-3xl font-bold my-10 pb-20">원하는 NFT 프로젝트 맞춤 알림 서비스</h2>
-    <div className="w-full text-center pt-10">
+    <h1 className="w-full text-center text-6xl font-bold text-black-500 pt-20">NFT NOTICE</h1>
+    <h2 className="w-full text-center text-3xl font-bold my-10 pb-24">원하는 NFT 프로젝트 맞춤 알림 서비스</h2>
+    <div className="w-full text-center">
       <Link href="/subscribe">
         <button className="bg-gray-300 hover:bg-gray-400 w-32 h-10 rounded-md ">구독하기</button>
       </Link>
     </div>
-    <div className="w-full text-center pt-10">
+    <div className="w-full text-center pt-8">
       <Link href="/subscribe_manage">
         <button className="bg-gray-300 hover:bg-gray-400 w-32 h-10 rounded-md">구독변경</button>
       </Link>
     </div>
-    <div className="w-full text-center pt-10">
+    <div className="w-full text-center pt-8">
       <Link href="/project_login">
         <button className="bg-gray-300 hover:bg-gray-400 w-32 h-10 rounded-md">프로젝트 로그인</button>
       </Link>
+    </div>
+    <div className="w-full text-center pt-24 text-black underline">
+      <Link href="/temporary">문의: jason@nftnotice.com</Link>
     </div>
   </Layout>
 );
--- src/pages/subscribe_manage.tsx
+++ src/pages/subscribe_manage.tsx
@@ -4,7 +4,39 @@ import Link from 'next/link';
 
 const Home: NextPage = () => (
   <Layout>
-    <h1 className="w-full text-center text-6xl font-bold text-black-500 pt-10">구독관리</h1>
+    <h1 className="w-full text-center text-6xl font-bold text-black-500 pt-20">구독관리</h1>
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex items-center justify-center">
+        <div className="flex h-10 w-20 mt-24 items-center justify-center">이메일</div>
+        <div className="border-2 border-gray-300 h-10 w-64 p-2 mt-24 flex items-center rounded-md">
+          <input
+            type="email"
+            name="email"
+            placeholder="이메일을 입력해주세요"
+            className="placeholder:text-gray-500 outline-none text-md flex-1"
+          />
+        </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="flex h-10 w-20 mt-4 items-center justify-center">비밀번호</div>
+        <div className="border-2 border-gray-300 h-10 w-64 p-2 mt-4 flex items-center rounded-md">
+          <input
+            type="email"
+            name="email"
+            placeholder="4자리 입력(설정한 경우)"
+            className="placeholder:text-gray-500 outline-none text-md flex-1"
+          />
+        </div>
+      </div>
+      <div>
+        <Link href="/temporary">
+          <button className="bg-gray-600 text-white hover:bg-gray-400 w-24 h-10 mt-20 rounded-md ">로그인</button>
+        </Link>
+      </div>
+      <div className="w-full text-center pt-24 text-black underline">
+        <Link href="/temporary">비밀번호를 잊으셨나요?</Link>
+      </div>
+    </div>
   </Layout>
 );
 
